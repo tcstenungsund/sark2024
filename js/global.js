8730cb8eed0daec4c5a1cda0db3da56ff04e945e
Kenneth Frantzen <5168530+seetee@users.noreply.github.com>
2024-02-20 13:27:17 +0100

    Initial commit

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.sark2024 = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
